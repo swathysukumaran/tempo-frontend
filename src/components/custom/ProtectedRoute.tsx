7189c395d5ed343282c84db63078a3024efdb9b7
swathysukumaran <swathysukumaran@gmail.com>
2025-04-24 15:49:10 -0700

    Debug protected route

--- src/components/custom/ProtectedRoute.tsx
+++ src/components/custom/ProtectedRoute.tsx
@@ -5,12 +5,14 @@ import { Navigate, useLocation } from "react-router-dom";
 function ProtectedRoute({ children }: { children: JSX.Element }) {
   const [isAuthenticated, setIsAuthenticated] = useState<boolean | null>(null);
   const location = useLocation();
+  console.log("in protected route", location.pathname);
   useEffect(() => {
     const checkAuth = async () => {
       try {
         const res = await fetch(`${API_URL}/me`, {
           credentials: "include",
         });
+        console.log("Response:", res);
         setIsAuthenticated(res.ok);
       } catch (err) {
         console.log(err);
