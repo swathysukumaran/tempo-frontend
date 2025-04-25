392e1716fde38f9cb70d83dcebdc9c4026d70a04
swathysukumaran <swathysukumaran@gmail.com>
2025-04-24 21:41:21 -0700

    Fix background color

--- src/components/custom/tripDetails.tsx
+++ src/components/custom/tripDetails.tsx
@@ -598,7 +598,7 @@ function TripDetails() {
             className={`px-4 py-2 text-sm font-medium transition-colors ${
               activeTab === "hotels"
                 ? "text-primary border-b-2 border-primary"
-                : "text-gray-500 hover:text-gray-700"
+                : "text-gray-500 hover:text-gray-700  bg-white"
             }`}
           >
             <Hotel className="inline-block mr-2 -mt-1" size={20} />
